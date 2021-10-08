679503f6845986110d8e95e9408b1b7aaea9978a
Nilo Jatico II <niloii.jatico@gmail.com>
2021-10-08 21:14:46 +0800

    Added code for Pokemons.

--- Pokemon.c
+++ Pokemon.c
@@ -3,7 +3,13 @@
 
 typedef char String[21];
 
+typedef struct attackTag {
+	String name;
+	int pp, accuracy;
+} Attack;
+
 typedef struct pokemonTag {
+	Attack attackStyle[3];
 	String name;
 	int type, hp, ap;
 } Pokemon;
@@ -14,13 +20,157 @@ typedef struct playerTag {
 } Player;
 
 /*
-	FUNCTION: setPlayerNames
-	PURPOSE: The function asks the user the name of the user and the name of the user's opponent
-	PRE-CONDITION: The user must accept that he or she must play the game.
-	POST-CONDITION: The user will then set the names of the user and the opponent.
+	FUNCTION NAME: initializeAttackTypes
+	DESCRIPTION: The function sets up the attack types for each Pokemon.
 	PARAMETERS:
-		1. *user - Player struct that pertains to the user.
-		2. *cpu - Player struct that pertains to the opponent.
+		1. type      (int) holds the type of Pokemon.
+		2. attacks[] (Attack) is a struct variable that holds information on the attack.
+	RETURN VALUE: None
+*/
+
+void initializeAttackTypes(int type, Attack attacks[]) {
+	if(type == 1) {
+		strcpy(attacks[0].name, "Leaf Storm");
+		attacks[0].pp = 130;
+		attacks[0].accuracy = 90;
+		
+		strcpy(attacks[1].name, "Mega Drain");
+		attacks[1].pp = 50;
+		attacks[1].accuracy = 100;
+		
+		strcpy(attacks[2].name, "Razor Leaf");
+		attacks[2].pp = 55;
+		attacks[2].accuracy = 95;
+	}
+	else if(type == 2) {
+		strcpy(attacks[0].name, "Ember");
+		attacks[0].pp = 60;
+		attacks[0].accuracy = 100;
+		
+		strcpy(attacks[1].name, "Fire Punch");
+		attacks[1].pp = 85;
+		attacks[1].accuracy = 80;
+		
+		strcpy(attacks[2].name, "Flame Wheel");
+		attacks[2].pp = 70;
+		attacks[2].accuracy = 90;
+	}
+	else {
+		strcpy(attacks[0].name, "Bubble");
+		attacks[0].pp = 40;
+		attacks[0].accuracy = 100;
+		
+		strcpy(attacks[1].name, "Hydro Pump");
+		attacks[1].pp = 185;
+		attacks[1].accuracy = 30;
+		
+		strcpy(attacks[2].name, "Surf");
+		attacks[2].pp = 70;
+		attacks[2].accuracy = 90;
+	}
+}
+
+/*
+	FUNCTION NAME: initializePokemons
+	DESCRIPTION: The function sets up the Pokemons.
+	PARAMETERS:
+		1. pokemons[] (Pokemon) is a struct variable that holds information on the Pokemon.
+	RETURN VALUE: None
+*/
+
+void initializePokemons(Pokemon pokemons[]) {
+	int i;
+	
+	strcpy(pokemons[0].name, "Bulbasaur");
+	pokemons[0].type = 1;
+	pokemons[0].hp = 160;
+	pokemons[0].ap = 40;
+	
+	strcpy(pokemons[1].name, "Bellsprout");
+	pokemons[1].type = 1;
+	pokemons[1].hp = 140;
+	pokemons[1].ap = 60;
+	
+	strcpy(pokemons[2].name, "Oddish");
+	pokemons[2].type = 1;
+	pokemons[2].hp = 150;
+	pokemons[2].ap = 50;
+	
+	strcpy(pokemons[3].name, "Charmander");
+	pokemons[3].type = 2;
+	pokemons[3].hp = 125;
+	pokemons[3].ap = 70;
+	
+	strcpy(pokemons[4].name, "Ninetails");
+	pokemons[4].type = 2;
+	pokemons[4].hp = 130;
+	pokemons[4].ap = 50;
+	
+	strcpy(pokemons[5].name, "Ponyta");
+	pokemons[5].type = 2;
+	pokemons[5].hp = 140;
+	pokemons[5].ap = 60;
+	
+	strcpy(pokemons[6].name, "Squirtle");
+	pokemons[6].type = 3;
+	pokemons[6].hp = 180;
+	pokemons[6].ap = 20;
+	
+	strcpy(pokemons[7].name, "Psyduck");
+	pokemons[7].type = 3;
+	pokemons[7].hp = 170;
+	pokemons[7].ap = 40;
+	
+	strcpy(pokemons[8].name, "Seel");
+	pokemons[8].type = 3;
+	pokemons[8].hp = 150;
+	pokemons[8].ap = 50;
+	
+	for(i = 0; i < 9; i++) {
+		initializeAttackTypes(pokemons[i].type, pokemons[i].attackStyle);
+	}
+}
+
+/*
+	FUNCTION NAME: displayPokemonList
+	DESCRIPTION: The function displays the information on the available Pokemon.
+	PARAMETERS:
+		1. pokemons[] (Pokemon) is a struct variable that contains the information of the Pokemon.
+	RETURN VALUE: None
+*/
+
+void displayPokemonList(Pokemon pokemons[]) {
+	int i;
+	
+	for(i = 0; i < 9; i++) {
+		printf("Name: %s\n", pokemons[i].name);
+		printf("Health Points: %d \t Attack Points: %d \n\n", pokemons[i].hp, pokemons[i].ap);
+	}
+}
+
+/*
+	FUNCTION NAME: chooseUserPokemon
+	DESCRIPTION: The function asks the user for their choice of Pokemon.
+	PARAMETERS:
+		1. user *(Player) is the struct variable containing the information of the player.
+		2. cpu   (Pokemon) is the struct variable containing the information of the Pokemon.
+	RETURN VALUE: None
+*/
+
+void chooseUserPokemon(Player *player, Pokemon pokemons[]) {
+	system("@cls||clear");
+	printf("There are nine Pokemons in total.\n");
+	printf("Please choose three out of the nine.\n\n");
+	displayPokemonList(pokemons);
+}
+
+/*
+	FUNCTION NAME: setPlayerNames
+	DESCRIPTION: The function asks the user for the names.
+	PARAMETERS:
+		1. user *(Player) is the struct variable containing the information of the player.
+		2. cpu  *(Player) is the struct variable containing the information of the opponent.
+	RETURN VALUE: None
 */
 
 void setPlayerNames(Player *user, Player *cpu) {
@@ -46,11 +196,10 @@ void setPlayerNames(Player *user, Player *cpu) {
 }
 
 /*
-	FUNCTION: printEndingMessage
-	PURPOSE: The function prints a message to the user that the program will terminate.
-	PRE-CONDITION: The user must have exited the game from the main menu.
-	POST-CONDITION: None
+	FUNCTION NAME: printEndingMessage
+	DESCRIPTION: The function prints out the message before terminating the program.
 	PARAMETERS: None
+	RETURN VALUE: None
 */
 
 void printEndingMessage() {
@@ -58,11 +207,10 @@ void printEndingMessage() {
 }
 
 /*
-	FUNCTION: printMainMenu
-	PURPOSE: The function prints two options for the user to choose in the main menu.
-	PRE-CONDITION: None
-	POST-CONDITION: This will determine whether the game will start or not.
+	FUNCTION NAME: printMainMenu
+	DESCRIPTION: The function prints out the main menu for the user.
 	PARAMETERS: None
+	RETURN VALUE: 1 - Play || 2 - Exit
 */
 
 int printMainMenu() {
@@ -82,11 +230,10 @@ int printMainMenu() {
 }
 
 /*
-	FUNCTION: printPokemonLogo
-	PURPOSE: The function prints the ASCII version of the Pokemon logo.
-	PRE-CONDITION: None
-	POST-CONDITION: None
+	FUNCTION NAME: printPokemonLogo
+	DESCRIPTION: The function prints out the ASCII Pokemon logo.
 	PARAMETERS: None
+	RETURN VALUE: None
 */
 
 void printPokemonLogo() {
@@ -107,12 +254,15 @@ void printPokemonLogo() {
 int main() {
 	int gameMode;
 	Player user, cpu;
+	Pokemon pokemons[9];
 	
 	printPokemonLogo();
 	gameMode = printMainMenu();
 	
 	if(gameMode == 1) {
 		setPlayerNames(&user, &cpu);
+		initializePokemons(pokemons);
+		chooseUserPokemon(&user, pokemons);
 	}
 	else {
 		printEndingMessage();
